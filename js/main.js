7370e0c30d26a97bfe5eaf746b82114680c45eac
Uriel Alvarez Valdez <azaki14@gmail.com>
2020-05-16 00:55:22 -0500

    Proyect complete

--- js/main.js
+++ js/main.js
@@ -1,27 +1,79 @@
 (function () {
   "use strict";
-  console.log("toy rdy");
   window.addEventListener("DOMContentLoaded", () => {
-    const elementosSlider = document.querySelectorAll(
+    // get all of elements on slider in one array
+    const elementosSlider = [...document.querySelectorAll(
       "#slider-container .slider-element"
-    );
+    )];
+    let sliderLenght = elementosSlider.length;
     const sliderButtons = document.querySelectorAll(".btn");
+
+    //Listener for buttons
     sliderButtons.forEach((prev) => {
       prev.addEventListener("click", slide);
     });
-
+    //function for slider events
     function slide(e) {
       event.stopPropagation();
+      //If click target is the img of button
       if (e.target.getAttribute("src")) {
         let button = e.target.parentNode.classList.contains("next");
         if (button === true) {
-          let elementActive = elementosSlider.indexOf("active");
-          console.log(elementActive);
+          let elementActive = document.querySelector(".active");
+          const indexActive = elementosSlider.indexOf(elementActive);
+          const nextSlider = elementosSlider[indexActive+1];
+          const sliderInitial = elementosSlider[0];
+          const prevSlider = elementosSlider[indexActive-1];
+          if (indexActive+1 < sliderLenght) {
+            elementActive.classList.remove("active");
+            nextSlider.classList.add('active');
+          }
+          else{
+            elementActive.classList.remove("active");
+            sliderInitial.classList.add("active");
+          }
         } else {
+          let elementActive = document.querySelector(".active");
+          const indexActive = elementosSlider.indexOf(elementActive);
+          const sliderInitial = elementosSlider[0];
+          const prevSlider = elementosSlider[indexActive-1];
+          if(indexActive > 0){
+            elementActive.classList.remove('active');
+            prevSlider.classList.add("active");
+          }
+          else{
+            elementActive.classList.remove('active');
+            elementosSlider[sliderLenght-1].classList.add('active');
+          }
         }
-      } else {
+      }/*Same proces but with target on the button*/ else {
+        let elementActive = document.querySelector(".active");
+        const indexActive = elementosSlider.indexOf(elementActive);
+        const nextSlider = elementosSlider[indexActive+1];
+        const sliderInitial = elementosSlider[0];
+        //If the button is next
         if (e.target.classList.contains("next")) {
+          //if active is < than the elements on the slider for detec when you are at the final element
+          if (indexActive+1 < sliderLenght) {
+            elementActive.classList.remove("active");
+            nextSlider.classList.add('active');
+          }
+          else{
+            elementActive.classList.remove("active");
+            sliderInitial.classList.add("active");
+          }
         } else {
+          let elementActive = document.querySelector(".active");
+          const indexActive = elementosSlider.indexOf(elementActive);
+          const prevSlider = elementosSlider[indexActive-1];
+          if(indexActive > 0){
+            elementActive.classList.remove('active');
+            prevSlider.classList.add("active");
+          }
+          else{
+            elementActive.classList.remove('active');
+            elementosSlider[sliderLenght-1].classList.add('active');
+          }
         }
       }
     }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Slider page with pure Css and JavaScript 
+
+
+## This slider page is from forntend mentor
+
+The initial desing was tooked from frontend mentor page
+this slider widths were set to 1440px for desktop and 367px for mobile
+I make this slider only with the screenshot of this design because I don't have the project file
+
+### Original and my final design
