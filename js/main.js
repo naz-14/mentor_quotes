4dbe9f44b50cb86d1e7564b6127fde56d99968aa
Uriel Alvarez <azaki14@gmail.com>
2020-05-17 15:16:40 -0500

    All working and readme added-final

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
   "use strict";
   window.addEventListener("DOMContentLoaded", () => {
     // get all of elements on slider in one array
-    const elementosSlider = [...document.querySelectorAll(
-      "#slider-container .slider-element"
-    )];
-    let sliderLenght = elementosSlider.length;
+    const elementosSlider = [
+      ...document.querySelectorAll("#slider-container .slider-element"),
+    ];
+    const sliderLenght = elementosSlider.length;
     const sliderButtons = document.querySelectorAll(".btn");
 
     //Listener for buttons
@@ -17,62 +17,58 @@
       event.stopPropagation();
       //If click target is the img of button
       if (e.target.getAttribute("src")) {
-        let button = e.target.parentNode.classList.contains("next");
+        const button = e.target.parentNode.classList.contains("next");
         if (button === true) {
-          let elementActive = document.querySelector(".active");
+          const elementActive = document.querySelector(".active");
           const indexActive = elementosSlider.indexOf(elementActive);
-          const nextSlider = elementosSlider[indexActive+1];
+          const nextSlider = elementosSlider[indexActive + 1];
           const sliderInitial = elementosSlider[0];
-          const prevSlider = elementosSlider[indexActive-1];
-          if (indexActive+1 < sliderLenght) {
+          const prevSlider = elementosSlider[indexActive - 1];
+          if (indexActive + 1 < sliderLenght) {
             elementActive.classList.remove("active");
-            nextSlider.classList.add('active');
-          }
-          else{
+            nextSlider.classList.add("active");
+          } else {
             elementActive.classList.remove("active");
             sliderInitial.classList.add("active");
           }
         } else {
-          let elementActive = document.querySelector(".active");
+          const elementActive = document.querySelector(".active");
           const indexActive = elementosSlider.indexOf(elementActive);
           const sliderInitial = elementosSlider[0];
-          const prevSlider = elementosSlider[indexActive-1];
-          if(indexActive > 0){
-            elementActive.classList.remove('active');
+          const prevSlider = elementosSlider[indexActive - 1];
+          if (indexActive > 0) {
+            elementActive.classList.remove("active");
             prevSlider.classList.add("active");
-          }
-          else{
-            elementActive.classList.remove('active');
-            elementosSlider[sliderLenght-1].classList.add('active');
+          } else {
+            elementActive.classList.remove("active");
+            elementosSlider[sliderLenght - 1].classList.add("active");
           }
         }
-      }/*Same proces but with target on the button*/ else {
-        let elementActive = document.querySelector(".active");
+      } /*Same proces but with target on the button*/ else {
+        const elementActive = document.querySelector(".active");
         const indexActive = elementosSlider.indexOf(elementActive);
-        const nextSlider = elementosSlider[indexActive+1];
+        const nextSlider = elementosSlider[indexActive + 1];
         const sliderInitial = elementosSlider[0];
         //If the button is next
         if (e.target.classList.contains("next")) {
           //if active is < than the elements on the slider for detec when you are at the final element
-          if (indexActive+1 < sliderLenght) {
+          if (indexActive + 1 < sliderLenght) {
             elementActive.classList.remove("active");
-            nextSlider.classList.add('active');
-          }
-          else{
+            nextSlider.classList.add("active");
+          } else {
             elementActive.classList.remove("active");
             sliderInitial.classList.add("active");
           }
         } else {
-          let elementActive = document.querySelector(".active");
+          const elementActive = document.querySelector(".active");
           const indexActive = elementosSlider.indexOf(elementActive);
-          const prevSlider = elementosSlider[indexActive-1];
-          if(indexActive > 0){
-            elementActive.classList.remove('active');
+          const prevSlider = elementosSlider[indexActive - 1];
+          if (indexActive > 0) {
+            elementActive.classList.remove("active");
             prevSlider.classList.add("active");
-          }
-          else{
-            elementActive.classList.remove('active');
-            elementosSlider[sliderLenght-1].classList.add('active');
+          } else {
+            elementActive.classList.remove("active");
+            elementosSlider[sliderLenght - 1].classList.add("active");
           }
         }
       }
--- readme.md
+++ readme.md
@@ -1,10 +1,17 @@
-# Slider page with pure Css and JavaScript 
-
+# Slider page with pure Css and JavaScript https://affectionate-hermann-99b382.netlify.app/
 
 ## This slider page is from forntend mentor
 
-The initial desing was tooked from frontend mentor page
-this slider widths were set to 1440px for desktop and 367px for mobile
-I make this slider only with the screenshot of this design because I don't have the project file
+The initial desing was tooked from frontend mentor page.<br>
+Thes slider widths were set to 1440px for desktop and 367px for mobile.<br>
+I make this slider only with the screenshot of this design because I don't have the project file<br>
 
 ### Original and my final design
+
+You can found my final design at this link https://affectionate-hermann-99b382.netlify.app/ <br>
+This proyect can work with any number of item on the slider and on any other proyect if you respect the id and classes of the index.
+
+This is the original design<br>
+
+<img src="img/desktop-design-slide.jpg" width="500px" height="300px"> <br>
+<img src="img/mobile-design-slide.jpg" width="300px" height="500px">
